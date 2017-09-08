a56d5bcfefd52975de69daa426b53210c28615da
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-09-08 14:34:52 -0700

    Added cookie debug statement to index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/debug', function (req, res) {
-  res.status('200').send('Hello, world!');
+  res.status('200').send('Cookies in request: ' + req.get('Cookie'));
 });
 
 // This is a funny route we made to customize the look/feel of the app, it
